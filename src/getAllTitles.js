3fc76f9fe663914ff95c316e015ef37a57d2ba15
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-04 13:02:46 -0300

    first commit requirement 01

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,8 +1,15 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+  arrTitulos = []
+  movies.forEach( (item, index, array) => {
+    arrTitulos.push(item.titulo);
+  })
+  return arrTitulos
+
+
 }
+console.log(getAllTitles(movies))
 
 module.exports = {
   getAllTitles
