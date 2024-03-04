eabbbed56d74b015cb3c2c240f04aaef83faad49
Luana Dias <diasluana083@gmail.com>
2024-03-04 15:23:59 -0300

    requisito 02 - feito

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,9 +1,14 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
-}
+  if (id === undefined || typeof id !== 'number') {
+    return undefined;
+  }
+  const movie = movies.find(movie => movie.id === id);
 
+  return movie !== undefined ? movie : undefined
+}
+console.log(getMovieById(1))
 module.exports = {
   getMovieById
 }
