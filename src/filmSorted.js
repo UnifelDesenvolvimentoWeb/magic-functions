c290c72272857530abdaf76e364ec44e3994cb5a
Laiz <laizsc.89@gmail.com>
2024-03-04 17:58:05 -0300

    Requisito 06

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -1,6 +1,14 @@
 const movies = require("../data/movies");
 
 const filmSorted = (movies) => {
+
+  if (movies.length === 0) {
+    return [];
+  } else {
+    const movie = [...movies];
+    movie.sort((a, b) => a.anoLancamento - b.anoLancamento);
+    return movie;
+  }
   
 }
 
