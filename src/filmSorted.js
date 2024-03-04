aa474f5d031c190828cc8e520982c9344b79e473
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-03-04 16:28:46 -0300

    Projeto magic functions - requisito 06 - Done!

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -1,6 +1,12 @@
 const movies = require("../data/movies");
 
 const filmSorted = (movies) => {
+
+  const sortedMovies = [...movies];
+
+  sortedMovies.sort((a, b) => a.anoLancamento - b.anoLancamento);
+
+  return sortedMovies;
   
 }
 
