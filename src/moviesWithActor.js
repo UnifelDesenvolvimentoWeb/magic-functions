c3d67b2c0cf70d3d93a7c964a76097d9236a7a24
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-05 14:55:52 -0300

    Requirement 05

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,18 +1,15 @@
 const movies = require("../data/movies");
 
 function moviesWithActor(ator) {
-  /*const movie =  movies.find((item) => item.elenco === id)
-  return movie*/
+  
   let MovieList = []
   movies.forEach((item, index) => {
     let filtro = item.elenco.includes(ator) ? MovieList.push(item) : false
   })
-  return MovieList
 
-  //const filtro = movies.filter((item) => item.elenco[0] == "Leandro Firmino")
-  
+  return MovieList
 }
-console.log(moviesWithActor('Leigh'))
+
 module.exports = {
   moviesWithActor
 }
