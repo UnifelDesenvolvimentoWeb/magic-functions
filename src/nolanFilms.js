4e0d450b82ebe3931b68532cb2606781a1bf65d3
Laiz <laizsc.89@gmail.com>
2024-03-05 15:02:04 -0300

    Requisito 08

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,6 +1,10 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
+
+    const movieNolan = movies.filter((m) => m.diretor.includes("Christopher Nolan"));
+    movieNolan.sort((a, b) => b.avaliacao - a.avaliacao);
+    return movieNolan;
  
 }
 
