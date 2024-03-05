680a690b0e991512759add91ea62c92ee07eb722
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-05 14:03:07 -0300

    Requirement 04

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,9 +1,13 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
+  const lista =  movies.map((item) => item.avaliacao)
+  const maiorAV = lista.reduce((acc, curr) => acc > curr? acc : curr, 0)
+  const findFilm = movies.find((item) => item.avaliacao == maiorAV)
+  return  findFilm
 }
-
+const li = [] 
+console.log( bestRatedFilm(li))
 module.exports = {
   bestRatedFilm
 }
