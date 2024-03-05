ef3f927448cf8b36c81c7bb02c5bb904f8037267
Yago Passos <yagopassos98@hotmail.com>
2024-03-05 13:42:30 -0300

    Resolução do requisito 04

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
+  return movies.sort((a, b) => b.avaliacao - a.avaliacao).find((movie) => {
+    if (movie.avaliacao > 0){
+      return movie
+    }
+  })
 }
 
 module.exports = {
