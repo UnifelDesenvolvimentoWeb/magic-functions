cc80bb83acc90b2c18cc63645d675cc6e559360d
Yago Passos <yagopassos98@hotmail.com>
2024-03-06 14:28:32 -0300

    Resolução do requisito 5

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,7 +1,9 @@
 const { movies } = require("../data/list1");
 
 const titlesAndDirectors = (movies) => {
- 
+  movies.map((movie) => {
+    console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`)
+  })
 }
 
 module.exports = {
