7c18212dcecc394be347c0432d0240ef82abad38
Alexandro Oliveira <alexandroliveira84@gmail.com>
2024-03-06 18:42:26 -0300

    código 3 rodando

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,9 +1,16 @@
 const movies = require("../data/movies");
 
-function moviesWithActor(ator) {
- 
+function moviesWithActor(actor) {
+  
+  if (typeof actor !== 'string' || actor.trim() === '') {
+    return [];
+  }
+
+  const moviesWithGivenActor = movies.filter(movie => movie.elenco && movie.elenco.includes(actor));
+
+  return moviesWithGivenActor;
 }
 
 module.exports = {
   moviesWithActor
-}
+};
