ad047c6c48e7dfe1b135eb8d68349c46a01d0e2c
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-06 15:37:12 -0300

    requirement 07

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,9 +1,11 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
-}
 
+  const av = movies.filter((element) => element.disponivel == true).map((elemant) => elemant.titulo)
+  return av
+}
+filmsAvailable(movies)
 module.exports = {
   filmsAvailable
 }
