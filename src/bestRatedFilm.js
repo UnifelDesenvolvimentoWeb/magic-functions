3dc6752da328598e94babb24cc65b23b75e76080
Luana Dias <diasluana083@gmail.com>
2024-03-06 14:25:06 -0300

    Requisito 04 - feito

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,9 +1,20 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
-}
+  if (movies.length === 0) {
+    return undefined;
+  }
+  let bestFilm = movies[0];
 
+  for (let i = 1; i < movies.length; i++) {
+    if (movies[i].avaliacao > bestFilm.avaliacao) {
+      bestFilm = movies[i]
+    }
+  }
+  return bestFilm;
+}
+const bestFilm = bestRatedFilm(movies)
+console.log(bestFilm);
 module.exports = {
   bestRatedFilm
 }
