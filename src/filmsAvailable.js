2f8363f3f5bae3d76c73b1db99be217c12c08cb0
Yago Passos <yagopassos98@hotmail.com>
2024-03-06 14:58:23 -0300

    Resolução do requisito 7

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+  return movies.filter((movie) => {
+    return movie.disponivel === true
+  }).map((movie) => {
+    return movie.titulo
+  })
 }
 
 module.exports = {
