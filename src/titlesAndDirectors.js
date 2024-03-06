4858df9beb2dd7bcf09ebfa29fcd37bdc9f36285
Luana Dias <diasluana083@gmail.com>
2024-03-06 16:35:35 -0300

    Requisito 05 feito

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,7 +1,17 @@
 const { movies } = require("../data/list1");
 
 const titlesAndDirectors = (movies) => {
- 
+  if (movies.length === 0 ) {
+    return;
+  }
+
+  movies.forEach(movie => {
+    if (movie.titulo && movie.diretor) {
+      console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`);
+    } else {
+      console.log("O filme não tem título ou diretor definidos.")
+    }
+  })
 }
 
 module.exports = {
