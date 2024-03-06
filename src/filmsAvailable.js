8ee83e1010859aec85d86d0649ab5a26305a2fbb
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-03-06 14:43:38 -0300

    Projeto magic functions - requisito 07 - Done!

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,6 +1,18 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
+
+  const availableMovies = movies.filter(movie => movie.disponivel)
+
+  
+  if (availableMovies.length === 0) {
+    return [] 
+  }
+
+  
+  const availableTitles = availableMovies.map(movie => movie.titulo)
+
+  return availableTitles
  
 }
 
