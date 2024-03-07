65625b4ebd1cd8209528c8626f8ff307563f0be2
Yago Passos <yagopassos98@hotmail.com>
2024-03-07 00:35:21 -0300

    Resolução do requisito 9

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -1,7 +1,16 @@
 const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
-  
+  const movie = movies.find((movie) => {
+    return movie.titulo === titulo
+  })
+  if(!movie){
+    return "Filme não encontrado."
+  }
+  if(!movie.elenco || movie.elenco.length === 0){
+    return "Filme não encontrado."
+  }
+  return movie.elenco.slice(0, 2)
 }
 
 module.exports = {
