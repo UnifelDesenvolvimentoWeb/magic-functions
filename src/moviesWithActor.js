ad4ab9bedc62155aeeff9aa574ca1a489d0b4451
Elias700 <elias_ribeiro07@outlook.com.br>
2024-03-06 21:03:52 -0300

    requisito 03

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,8 +1,24 @@
+
+
+
+
+
 const movies = require("../data/movies");
+
+
 
 function moviesWithActor(ator) {
  
+  const actor = movies.filter((movie) => {
+      return movie.elenco.includes(ator)
+  })  
+      return actor
 }
+
+console.log(moviesWithActor('Cary Elwes'));
+
+
+
 
 module.exports = {
   moviesWithActor
