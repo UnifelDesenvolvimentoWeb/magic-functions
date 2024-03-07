d87d81b65bf78bb97ef84392f44fdb477659d618
Luana Dias <diasluana083@gmail.com>
2024-03-07 16:55:38 -0300

    Requisito 09 - feito

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -1,8 +1,23 @@
 const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
-  
+  if (!titulo || typeof titulo !== 'string' || titulo.trim() === '') {
+    return "Filme não encontrado."
+  }
+  const movie = movies.find(movie => movie.titulo.toLowerCase() === titulo.toLocaleLowerCase())
+  if (!movie) {
+    return "Filme não encontrado."
+  }
+  if (!movie.elenco || movie.elenco.length <2) {
+    return "Elenco do filme não encontrado."
+  }
+  return movie.elenco.slice(0,2)
 }
+//Testes 
+console.log("Teste:");
+console.log(findActorForTheMovie("Matrix"));
+console.log("\nTeste 2:");
+console.log(findActorForTheMovie("Invalido"));
 
 module.exports = {
   findActorForTheMovie
