3077690899d3c7fccfae84e1a084bdf25b7c9391
Luana Dias <diasluana083@gmail.com>
2024-03-07 17:13:44 -0300

    Requisito 10 - feito

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,23 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+ const genresCount = {}
+
+  if (movies.lenght === 0) {
+   return genresCount
+}
+  movies.forEach((movie) => {
+    if (movie.genero && movie.genero.lenght >0) {
+      movie.genero.forEach((genre) => {
+        if (genresCount.hasOwnProperty(genre)) {
+          genresCount[genre]++
+        } else {
+          genresCount[genre] =1
+        }
+      })
+    }
+  })
+  return genresCount
 }
 
 module.exports = {
