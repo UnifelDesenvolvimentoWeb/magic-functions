5f1ff07ab0e2a33e4d1f6f892c0a852b176090fc
Alexandro Oliveira <alexandroliveira84@gmail.com>
2024-03-07 18:03:41 -0300

    código 7

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,9 +1,14 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+  
+  const availableMovies = movies.filter(movie => movie.disponivel === true);
+
+  const availableTitles = availableMovies.map(movie => movie.titulo);
+
+  return availableTitles;
 }
 
 module.exports = {
   filmsAvailable
-}
+};
