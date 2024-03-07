c709bd2b6f4ea816de004bc9c57436f0493151ba
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-03-07 15:09:33 -0300

    Projeto magic functions - Requisito 13 - Done!

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,6 +1,34 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
+
+  const currentDate = new Date()
+  const overdueLoans = []
+
+ 
+  for (const movie of movies) {
+    
+    if (movie.emprestimos && movie.emprestimos.length > 0) {
+      
+      for (const emprestimo of movie.emprestimos) {
+        
+        if (new Date(emprestimo.dataDevolucao) < currentDate) {
+          
+          emprestimo.status = "atrasado"
+          
+          overdueLoans.push({
+            id: movie.id,
+            titulo: movie.titulo,
+            dataEmprestimo: emprestimo.dataEmprestimo,
+            dataDevolucao: emprestimo.dataDevolucao,
+            nomePessoa: emprestimo.nomePessoa
+          })
+        }
+      }
+    }
+  }
+
+  return overdueLoans
  
 }
 
