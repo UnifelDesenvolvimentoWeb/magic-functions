1aad762d62ed0efd402c3748dd3882171f0f9313
Alexandro Oliveira <alexandroliveira84@gmail.com>
2024-03-07 17:42:05 -0300

    Código 6 aparentemente ok

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -2,8 +2,13 @@ const movies = require("../data/movies");
 
 const filmSorted = (movies) => {
   
+  const copiedMovies = [...movies];
+
+  copiedMovies.sort((a, b) => a.anoLancamento - b.anoLancamento);
+
+  return copiedMovies;
 }
 
 module.exports = {
   filmSorted
-}
+};
