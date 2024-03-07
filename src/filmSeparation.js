1d92520332ccbc944e76f9f8f96d401e971620de
Cayo Santana <102827350+cayosantana@users.noreply.github.com>
2024-03-07 14:59:16 -0300

    Projeto magic functions - Requisito 12 - Done!

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -2,10 +2,52 @@ const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
 
-  
+  const genderMovies = {}
 
+  
+  for (const movie of movies) {
+    
+    if (movie.genero?.length > 0) {
+      
+      for (const genre of movie.genero) {
+        
+        if (genderMovies[genre] === undefined) {
+          
+          genderMovies[genre] = []
+        }
+        
+        const itemsArray = details ? {
+          anoLancamento: movie.anoLancamento,
+          avaliacao: movie.avaliacao,
+          diretor: movie.diretor,
+          id: movie.id,
+          titulo: movie.titulo
+        } : movie.titulo
+        genderMovies[genre].push(itemsArray)
+      }
+    }
+  }
 
  
+  const genderMoviesOrd = {}
+  const genderOrd = Object.keys(genderMovies).sort()
+  for (const genero of genderOrd) {
+    genderMoviesOrd[genero] = details ? genderMovies[genero].sort((a, b) => {
+      const tituloA = a.titulo.toUpperCase() 
+      const tituloB = b.titulo.toUpperCase()
+
+      if (tituloA < tituloB) {
+          return -1
+      }
+      if (tituloA > tituloB) {
+          return 1
+      }
+      return 0 
+  }) : genderMovies[genero].sort()
+  }
+
+  return genderMoviesOrd
+
 }
 
 module.exports = {
