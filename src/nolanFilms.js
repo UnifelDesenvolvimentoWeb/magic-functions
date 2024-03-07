9a2b1d73a16665e291ac18c236815f7a6752be50
Luana Dias <diasluana083@gmail.com>
2024-03-07 16:39:50 -0300

    Requisito 08 - feito

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,8 +1,15 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
- 
+ const nolanMovies = movies.filter(movie => movie.diretor === "Christopher Nolan")
+ const sortedMovies = nolanMovies.sort((a, b) => b.avaliacao - a.avaliacao)
+ return sortedMovies
 }
+const sortedNolanMovies = nolanFilms(movies);
+console.log("Filmes dirigidos por Christopher Nolan, ordenados pela avaliação em ordem decrescente:");
+sortedNolanMovies.forEach(movie => {
+  console.log(`Título: ${movie.titulo} - Avaliação: ${movie.avaliacao}`);
+});
 
 module.exports = {
   nolanFilms
