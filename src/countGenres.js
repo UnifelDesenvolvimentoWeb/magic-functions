102152db07002b8d35961b75ccc383d65dbf17d6
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-08 15:19:20 -0300

    Requirement 10

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,9 +1,18 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  contGenero = {}
+  for(let movie of movies){
+    for(let gener of movie.genero){
+      if(contGenero[gener] == undefined){
+        contGenero[gener] = 0
+      }
+      contGenero[gener]++
+    }      
+  }
+  return contGenero
 }
-
+console.log(countGenres(movies))
 module.exports = {
   countGenres
 }
