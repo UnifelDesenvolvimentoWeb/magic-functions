88bc58649e974b4b287ab1d06073715c1cf60f6d
Uelber20 <uelber.e.s.a@gmail.com>
2024-03-08 19:59:32 -0300

    Primeiro requisito

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -2,8 +2,16 @@ const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
  
-}
+const todosTitulos = movies.map((movie) => {
 
+return movie.titulo
+
+})
+
+return todosTitulos
+
+}
+console.log(getAllTitles(movies));
 module.exports = {
   getAllTitles
 }
