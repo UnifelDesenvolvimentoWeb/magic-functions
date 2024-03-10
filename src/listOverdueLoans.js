cc799d161f590736c25d6b202f27eae6920ce45b
Alexandro Oliveira <alexandroliveira84@gmail.com>
2024-03-10 10:28:46 -0300

    Código 13

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,7 +1,29 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+
+  const currentDate = new Date();
+  const overdueRentals = [];
+
+  for (const movie of movies) {
+    if (movie.emprestimos && movie.emprestimos.length > 0) {
+      for (const loan of movie.emprestimos) {
+        if (new Date(loan.dataDevolucao) < currentDate) {
+          loan.status = "atrasado";
+          overdueRentals.push({
+            id: movie.id,
+            titulo: movie.titulo,
+            dataEmprestimo: loan.dataEmprestimo,
+            dataDevolucao: loan.dataDevolucao,
+            nomePessoa: loan.nomePessoa
+          });
+        }
+      }
+    }
+  }
+
+  return overdueRentals;
+
 }
 
 module.exports = {
