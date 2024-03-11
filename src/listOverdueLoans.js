9d46afc29fa37326e9ed1d02ec8b4763082baf58
Luana Dias <diasluana083@gmail.com>
2024-03-11 19:06:52 -0300

    Requisito 13 - feito

--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,9 +1,30 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+ const currentDate = new Date();
+ currentDate.setHours(0, 0, 0, 0); 
+ const overdueLoans = [];
+
+ movies.forEach(movie => {
+ if (movie.emprestimos && movie.emprestimos.length > 0) {
+    movie.emprestimos.forEach(loan => {
+      const loanReturnDate = new Date(loan.dataDevolucao);
+      loanReturnDate.setHours(0, 0, 0, 0); 
+      if (loanReturnDate < currentDate) {
+        overdueLoans.push({
+          id: movie.id,
+          titulo: movie.titulo,
+          dataEmprestimo: loan.dataEmprestimo,
+          dataDevolucao: loan.dataDevolucao,
+          nomePessoa: loan.nomePessoa 
+        });
+      }
+    })
+ }
+ });
+ return overdueLoans;
 }
 
 module.exports = {
-  listOverdueLoans
-}
+ listOverdueLoans
+};
