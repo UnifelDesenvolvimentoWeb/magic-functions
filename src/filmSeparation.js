1d798ae09b0eaa1360a3d6deb1dd74bc6405b6ee
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-03-11 12:29:57 -0300

    requiriment 12 80% Doing

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,9 +1,40 @@
 const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
- 
-}
+  const filmPorgenero = {}
+  let listaGeneros = []
+  
+  for(movie of movies){
+    for(i of movie.genero){
+      if(!listaGeneros.includes(i)){
+        listaGeneros.push(i)
+        let lis = movies.filter((item) => item.genero.includes(i))
+        lis.sort()
+        
+        let SoTitulo = lis.map((item) => item.titulo)
+        SoTitulo.sort()
+        
+        if(details){
+          filmPorgenero[i] = lis
+        }
+        else{
+          filmPorgenero[i] = SoTitulo
 
+        }
+      }
+      
+    }
+  }
+
+
+
+
+return filmPorgenero
+  
+
+}
+console.log(filmSeparation(movies))
+//filmSeparation(movies)
 module.exports = {
   filmSeparation
 }
