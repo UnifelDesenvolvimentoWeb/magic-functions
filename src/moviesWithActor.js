5442e22a5cf32b2f407f17b446ee611a3415985a
mgportnoi <mayanaportnoi@live.com>
2024-03-12 20:24:19 -0300

    Implementação 03
    Implementação de um código que encontre filmes que incluam um determinado ator em seu elenco.

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,9 +1,19 @@
-const movies = require("../data/movies");
+const movies = require( "../data/movies" );
 
-function moviesWithActor(ator) {
- 
+function moviesWithActor ( ator )
+{
+  if ( typeof ator !== "string" )
+  {
+    return [];
+  }
+
+  const matchingMovies = movies.filter( ( movie ) =>
+    movie.elenco.includes( ator )
+  );
+
+  return matchingMovies;
 }
 
 module.exports = {
-  moviesWithActor
-}
+  moviesWithActor,
+};
