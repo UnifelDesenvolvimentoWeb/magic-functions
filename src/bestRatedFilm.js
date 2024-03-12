75813f89f3fd24ff33de3124a74c12fc801860c6
AriovaldoSantS <153957898+AriovaldoSantS@users.noreply.github.com>
2024-03-12 18:19:52 -0300

    04

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,7 +1,14 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
+  let bestRated = movies[0];
+
+  for (let i = 1; i < movies.length; i++) {
+    if (movies[i].avaliacao > bestRated.avaliacao) {
+      bestRated = movies[i]
+    }
+  }
+  return bestRated;
 }
 
 module.exports = {
