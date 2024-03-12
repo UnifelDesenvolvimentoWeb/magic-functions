8b65233a7e8f8408d17e32a7fb647fb628752fca
mgportnoi <mayanaportnoi@live.com>
2024-03-12 19:37:12 -0300

    Implementação 01
    Recuperar títulos de filmes a partir de uma lista de filmes em formato de array.

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,9 +1,21 @@
 const movies = require("../data/movies");
 
-const getAllTitles = (movies) => {
- 
-}
+const getAllTitles = ( movies ) =>
+{
+  if ( !Array.isArray( movies ) || movies.length === 0 )
+  {
+    return [];
+  }
+
+  const titles = movies.map( ( movie ) => movie.titulo );
+
+  return titles;
+};
 
 module.exports = {
   getAllTitles
 }
+
+//Teste
+const titles = getAllTitles( movies );
+console.log( titles );
