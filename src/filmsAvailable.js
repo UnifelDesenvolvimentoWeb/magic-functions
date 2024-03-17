a8ef10adefd03982a1e68d5f23ddd13b99f02f69
Wellington Farias <tonw2farias@gmail.com>
2024-03-17 18:06:40 -0300

    Requisito 07

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,7 +1,18 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+
+  const available = movies.filter(movie => movie.disponivel)
+
+
+  if (available.length === 0) {
+    return [] 
+  }
+
+
+  const availableTitles = available.map(movie => movie.titulo)
+
+  return availableTitles
 }
 
 module.exports = {
