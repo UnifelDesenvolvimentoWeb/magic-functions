79db41be0232e0836c4d9c43c0d0c6bf5a25c1e7
Wellington Farias <tonw2farias@gmail.com>
2024-03-17 17:50:45 -0300

    Requisito 4

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,6 +1,14 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
+
+  if (movies.length === 0) {
+    return undefined;
+  }
+
+  const filmeClassificaçãoMax = movies.filter(filme => filme.avaliacao === Math.max(...movies.map(f => f.avaliacao)));
+
+  return filmeClassificaçãoMax[0];
   
 }
 
