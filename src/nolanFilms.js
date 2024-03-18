06c733dc5335e7316ec21d22cd1dfc0c1a1da1bc
AriovaldoSantS <153957898+AriovaldoSantS@users.noreply.github.com>
2024-03-18 16:08:11 -0300

    Nolan

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,8 +1,13 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
+  const nolan = movies.filter((element) => element.diretor == 'Christopher Nolan')
+  return nolan.sort((a,b) => b - a)
+  
  
 }
+
+
 
 module.exports = {
   nolanFilms
