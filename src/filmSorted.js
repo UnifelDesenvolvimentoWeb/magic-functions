69d32a4b120e2719e1bd274928c8d7d7ab62e595
AriovaldoSantS <153957898+AriovaldoSantS@users.noreply.github.com>
2024-03-18 15:43:37 -0300

    07

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -1,6 +1,12 @@
 const movies = require("../data/movies");
 
 const filmSorted = (movies) => {
+
+  const sorted = [...movies];
+
+  sorted.sort((a, b) => a.anoLancamento - b.anoLancamento);
+
+  return sorted;
   
 }
 
