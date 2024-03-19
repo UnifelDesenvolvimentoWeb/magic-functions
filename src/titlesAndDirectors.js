9d9c5c3e2bcf8b1daaf8c2daebeff78aa4d9e5ca
mgportnoi <mayanaportnoi@live.com>
2024-03-19 15:02:52 -0300

    Implementação 05
    Implementação de um código que imprime títulos e diretores de cada filme de uma lista de filmes.

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,9 +1,28 @@
-const { movies } = require("../data/list1");
+const { movies } = require( "../data/list1" );
 
-const titlesAndDirectors = (movies) => {
- 
-}
+const titlesAndDirectors = ( movies ) =>
+{
+  if ( !Array.isArray( movies ) )
+  {
+    throw new TypeError( "O parâmetro 'movies' deve ser um array" );
+  }
+
+  if ( movies.length === 0 )
+  {
+    return;
+  }
+
+  for ( const movie of movies )
+  {
+    if ( typeof movie !== 'object' || movie === null || !movie.titulo || !movie.diretor )
+    {
+      continue;
+    }
+
+    console.log( `Título: ${ movie.titulo } - Diretor: ${ movie.diretor }` );
+  }
+};
 
 module.exports = {
-  titlesAndDirectors
-}
+  titlesAndDirectors,
+};
