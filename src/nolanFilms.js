53fb8e1b624b1bc51a667d0414e120639f25756c
mgportnoi <mayanaportnoi@live.com>
2024-03-19 20:15:25 -0300

    Implementação 08
    Implementação de  um código que filtra e ordena uma lista de filmes e retorna apenas os filmes dirigidos por Christopher Nolan, ordenando pela classificação em ordem decrescente.

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,9 +1,14 @@
 const movies = require("../data/movies");
 
-const nolanFilms = (movies) => {
- 
-}
+const nolanFilms = ( movies ) =>
+{
+  const nolanMovies = movies.filter( movie => movie.diretor === 'Christopher Nolan' );
+
+  nolanMovies.sort( ( a, b ) => b.avaliacao - a.avaliacao );
+
+  return nolanMovies;
+};
 
 module.exports = {
   nolanFilms
-}
+};
