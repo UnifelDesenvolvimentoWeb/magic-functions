522c84ab401871178dd33a55db76b5083d1c10c7
mgportnoi <mayanaportnoi@live.com>
2024-03-19 20:59:26 -0300

    Implementação 10
    Implementação de um código que conte a ocorrência de gêneros em uma lista de filmes e retorna um objeto com a contagem de cada gênero.

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,9 +1,14 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  const generoCount = {
+  };
+  // Itera sobre a lista de filmes
+
+for ( const movie of movies ) { if ( movie.genero && movie.genero.length > 0 ) { for ( const genero of movie.genero ) { if ( generoCount[ genero ] ) { generoCount[ genero ]++; } else { generoCount[ genero ] = 1; }}}
 }
 
-module.exports = {
-  countGenres
-}
+return generoCount;
+};
+
+module.exports = { countGenres };
