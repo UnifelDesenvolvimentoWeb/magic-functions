9eb2b31969c1eac663d7a727391f9ff8c3a49d5e
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-21 16:18:32 -0300

    getMovieById

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,7 +1,10 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
+  let movie = movies.filter(e => {
+    return e.id === id;
+  })
+  return movie[0] || undefined;
 }
 
 module.exports = {
