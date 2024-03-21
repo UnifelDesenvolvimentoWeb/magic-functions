30059335a10f438f41a83db4a25fb4acfe5ee00f
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-21 16:23:45 -0300

    moviesWithActor

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 function moviesWithActor(ator) {
- 
+  let movie = movies.filter(e => {
+    return e.elenco.includes(ator);
+  })
+
+  return (movie || []) ;
 }
 
 module.exports = {
