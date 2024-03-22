0513fb777f66b2ba52db22d9e9fc03ec1003709d
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-22 16:44:32 -0300

    filmSeparation

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,7 +1,43 @@
 const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
- 
+  
+  let sortedFilms = {};
+  let genres = [];
+
+  movies.forEach(movie => {
+    genres.push(...movie.genero);
+  })
+
+  genres = [...new Set(genres)].sort();
+  genres.forEach(genre => sortedFilms[genre] = [])
+  
+  movies.forEach(movie => {
+
+    movie.genero.forEach(genre => {
+
+      if(sortedFilms[genre]) {
+
+        if(details) {
+          sortedFilms[genre].push({"anoLancamento": movie.anoLancamento, "avaliacao": movie.avaliacao, "diretor": movie.diretor, "id": movie.id, "titulo": movie.titulo});
+        } 
+        else {
+          sortedFilms[genre].push(movie.titulo);
+        }
+      }
+    })
+  })
+
+  for(let key in sortedFilms) {
+    if(details) {
+      sortedFilms[key] = sortedFilms[key].sort((a, b) => a.titulo.localeCompare(b.titulo));
+    }
+    else {
+      sortedFilms[key] = sortedFilms[key].sort();
+    }
+  } 
+
+  return sortedFilms;
 }
 
 module.exports = {
