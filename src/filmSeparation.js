ac1cb7b2976879f06b37cdbb7f62d750e8cc207b
mgportnoi <mayanaportnoi@live.com>
2024-03-22 19:18:21 -0300

    Implementação 12
    Implementação de um código que separa filmes por gênero e os ordena alfabeticamente por título.

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,9 +1,40 @@
-const movies = require("../data/movies");
+function filmSeparation ( movies, details = true )
+{
+  const separatedMovies = {};
 
-function filmSeparation(movies, details = true) {
- 
+  movies.forEach( movie =>
+  {
+    movie.genero.forEach( genre =>
+    {
+      if ( !separatedMovies[ genre ] )
+      {
+        separatedMovies[ genre ] = [];
+      }
+
+      if ( details )
+      {
+        separatedMovies[ genre ].push( {
+          titulo: movie.titulo,
+          anoLancamento: movie.anoLancamento,
+          avaliacao: movie.avaliacao,
+          diretor: movie.diretor,
+          id: movie.id
+        } );
+      } else
+      {
+        separatedMovies[ genre ].push( movie.titulo );
+      }
+    } );
+  } );
+
+  for ( const genre in separatedMovies )
+  {
+    separatedMovies[ genre ].sort( ( a, b ) => typeof a === 'string' ? a.localeCompare( b ) : a.titulo.localeCompare( b.titulo ) );
+  }
+
+  return separatedMovies;
 }
 
 module.exports = {
   filmSeparation
-}
+};
