8007fdfccaf4e0994999e53b26d8c9de4541c31d
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-22 16:17:41 -0300

    iniciando o projeto - requisito getAllTitles

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,8 +1,7 @@
 const movies = require("../data/movies");
 
-const getAllTitles = (movies) => {
- 
-}
+const getAllTitles = (movies) => movies.map((movie)=> movie.titulo)
+
 
 module.exports = {
   getAllTitles
