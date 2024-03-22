adcf941e68b2339bc63f23b6f754f95f50766cb7
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-22 14:42:28 -0300

    filmsAvailable

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+  let titles = [];
+  movies.forEach(e => {
+    if(e.disponivel) {titles.push(e.titulo)}
+  })
+  return titles;
 }
 
 module.exports = {
