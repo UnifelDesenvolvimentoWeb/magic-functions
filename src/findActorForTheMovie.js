0601201ba68b556aad532ce384553eff7ac17a75
GabrieIBro <gabriel.fsans@hotmail.com>
2024-03-22 14:55:49 -0300

    findActorForTheMovie

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -1,8 +1,10 @@
 const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
-  
+  let movie = movies.filter(movie => movie.titulo === titulo)[0];
+  return (movie) ? movie.elenco.splice(0, 2) : "Filme não encontrado.";
 }
+
 
 module.exports = {
   findActorForTheMovie
