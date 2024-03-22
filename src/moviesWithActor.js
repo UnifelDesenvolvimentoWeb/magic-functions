3be56b9ce600ee4b103704a3c063a5158b87d1b2
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-22 16:40:14 -0300

    Desafio moviesWithActor completo

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,7 +1,7 @@
 const movies = require("../data/movies");
 
 function moviesWithActor(ator) {
- 
+  return movies.filter((movie)=> movie.elenco.some((actor)=> actor===ator))
 }
 
 module.exports = {
