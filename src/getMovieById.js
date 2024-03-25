c1a7975f258aec116257e94f68ac9c9dfa3849ba
maila520 <mailasantos520@gmail.com>
2024-03-25 15:55:36 -0300

    desafio2 ok

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,8 +1,17 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
+
+  if (typeof id !== "number") {
+    return undefined;
+  }
+
+  const movies = movies.find ((filmes) => filmes.id === id) ;
+  return movies || undefined; 
   
 }
+
+console.log(getMovieById (movies)); 
 
 module.exports = {
   getMovieById
