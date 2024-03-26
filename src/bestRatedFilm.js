41f6c8a295ed88a69364f68663def9eb331508b3
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 14:02:50 -0300

    Desafio bestRatedFilm completo

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,7 +1,11 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  
+  if (movies.length === 0) {
+    return undefined
+  } else {
+    return movies.reduce((acc, movie)=>movie.avaliacao > acc.avaliacao ? movie : acc)
+  }
 }
 
 module.exports = {
