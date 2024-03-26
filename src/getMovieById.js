9904098fe7e4717d6b830f1a7deb2dfb9421b9a8
Jean Wilker <jeanwilkerss@gmail.com>
2024-03-26 00:26:05 -0300

    Finaliza função getMovieById

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,9 +1,15 @@
-const movies = require("../data/movies");
+const movies = require('../data/movies');
 
 function getMovieById(id) {
-  
+  if (typeof id !== 'number' || isNaN(id)) {
+    return undefined;
+  }
+
+  return movies.find(({ id: movieId }) => movieId === id);
 }
 
 module.exports = {
-  getMovieById
-}
+  getMovieById,
+};
+
+console.log(getMovieById(6));
