5e72616da26676db70267a1e93c3fe3e88fc8a6b
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 15:02:58 -0300

    Desafio filmsAvailable completo

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,8 +1,6 @@
 const movies = require("../data/movies");
 
-const filmsAvailable = (movies) => {
- 
-}
+const filmsAvailable = (movies) => movies.filter((movie)=> movie.disponivel === true).map((movie)=>movie.titulo)
 
 module.exports = {
   filmsAvailable
