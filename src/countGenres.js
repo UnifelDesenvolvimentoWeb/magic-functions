8ca569a524453df2ac629d191bf4218c10dda096
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 17:30:40 -0300

    Desafio countGenres completo

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,16 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  return movies.reduce((acc, movie)=>{
+    movie.genero.forEach(gen => {
+      if (acc[gen]) {
+        acc[gen] += 1
+      } else {
+        acc[gen] = 1
+      }
+    })
+    return acc
+  }, {})  
 }
 
 module.exports = {
