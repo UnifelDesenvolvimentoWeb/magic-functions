91a41caf5a6fa069c46cddbe360f5e5d7cd35fa8
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 14:38:37 -0300

    Desafio filmSorted completo

--- src/filmSorted.js
+++ src/filmSorted.js
@@ -1,8 +1,6 @@
 const movies = require("../data/movies");
 
-const filmSorted = (movies) => {
-  
-}
+const filmSorted = (movies) => sorted = movies.slice().sort((a,b)=> a.anoLancamento - b.anoLancamento)
 
 module.exports = {
   filmSorted
