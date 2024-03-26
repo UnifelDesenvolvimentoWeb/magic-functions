6fdab99bb85a84f853eec3f02724f985190627d8
Helena Estrela <estrelahelena7@gmail.com>
2024-03-26 13:46:59 -0300

    Requisito - 03

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,7 +1,8 @@
 const movies = require("../data/movies");
 
 function moviesWithActor(ator) {
- 
+  const filter = movies.filter((movie) => movie.ator === ator)
+  return filter
 }
 
 module.exports = {
