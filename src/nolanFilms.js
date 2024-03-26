2b5c70ca608949eb89a55c5934eb0307453b2c0d
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 15:26:52 -0300

    Desafio nolanFilms completo

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,8 +1,6 @@
 const movies = require("../data/movies");
 
-const nolanFilms = (movies) => {
- 
-}
+const nolanFilms = (movies) => movies.filter((movie)=> movie.diretor === 'Christopher Nolan').sort((a,b)=> b.avaliacao - a.avaliacao)
 
 module.exports = {
   nolanFilms
