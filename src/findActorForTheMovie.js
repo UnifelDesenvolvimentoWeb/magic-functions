16cb965f9072c6c3f7edebfdd852c728274c1d96
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 16:37:17 -0300

    Desafio findActorForTheMovie completo

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -1,7 +1,10 @@
 const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
-  
+  if (movies.some((movie)=> movie.titulo === titulo)){
+    return movies.find((movie)=> movie.titulo === titulo).elenco.slice(0,2)
+  }
+  return 'Filme não encontrado.'
 }
 
 module.exports = {
