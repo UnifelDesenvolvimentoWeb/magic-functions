23f00b06305c9bf7a841a149bd1803d15eef97a5
Jean Wilker <jeanwilkerss@gmail.com>
2024-03-26 01:00:23 -0300

    Finaliza função moviesWithSctor.

--- src/moviesWithActor.js
+++ src/moviesWithActor.js
@@ -1,9 +1,19 @@
-const movies = require("../data/movies");
+const movies = require('../data/movies');
 
 function moviesWithActor(ator) {
- 
+  const atorLowerCase = ator.toLowerCase();
+
+  const filmesComAtor = movies.filter((movie) => {
+    const elencoLowerCase = movie.elenco.map((ator) => ator.toLowerCase());
+
+    return elencoLowerCase.includes(atorLowerCase);
+  });
+
+  return filmesComAtor;
 }
 
 module.exports = {
-  moviesWithActor
-}
+  moviesWithActor,
+};
+
+console.log(moviesWithActor('Keanu Reeves'));
