46bb10a2e1717b5f6f86e8c54936d97721919c87
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-26 14:21:59 -0300

    Desafio titlesAndDirectors completo

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,7 +1,11 @@
 const { movies } = require("../data/list1");
 
 const titlesAndDirectors = (movies) => {
- 
+  if (movies.length !== 0) {
+    movies.forEach(movie => {
+      console.log(`Título: ${movie.titulo} - Diretor: ${movie.diretor}`);
+    });
+  }
 }
 
 module.exports = {
