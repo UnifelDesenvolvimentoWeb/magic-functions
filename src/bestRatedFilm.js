cd9a59ad893537c11193ab1e719e8e65aba5e50a
Helena Estrela <estrelahelena7@gmail.com>
2024-03-26 13:38:17 -0300

    Requisito - 02

--- src/bestRatedFilm.js
+++ src/bestRatedFilm.js
@@ -1,15 +1,10 @@
 const movies = require("../data/movies");
 
 const bestRatedFilm = (movies) => {
-  //sort
-  if (movies > 0) {
     const sorted = movies.sort((a, b) => {
-    b.avaliacao - a.avaliacao
+    a.avaliacao - b.avaliacao
     }) .find((movies) => movies.avaliacao > 0)
     return sorted
-  } else {
-    return undefined
-  }
 }
 
 module.exports = {
--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,7 +1,7 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-    const movie = movies.find((id) => movies.id === id)
+    const movie = movies.find((movie) => movie.id === id)
     return movie
 }
 
