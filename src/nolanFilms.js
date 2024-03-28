00a7a1eca60b542877a68c28a3680ce95555c74c
maila520 <mailasantos520@gmail.com>
2024-03-28 14:15:35 -0300

    desafio 08 ok

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,8 +1,11 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
+  return movies.filter((element) => element.diretor === 'Christopher Nolan')
+
  
 }
+console.log(nolanFilms(movies));
 
 module.exports = {
   nolanFilms
