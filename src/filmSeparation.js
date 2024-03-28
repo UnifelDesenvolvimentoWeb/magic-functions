77187a441032863c761e10d145173cf9bd1e9c31
rodrigo-csantos <rodrigosantos.20rsc@gmail.com>
2024-03-28 16:23:17 -0300

    Desafio filmSeparation completo

--- src/filmSeparation.js
+++ src/filmSeparation.js
@@ -1,7 +1,17 @@
 const movies = require("../data/movies");
 
 function filmSeparation(movies, details = true) {
- 
+  return movies.reduce((acc, movie)=>{
+    movie.genero.forEach((genero)=> {
+      const {anoLancamento, avaliacao, diretor, id, titulo} = movie
+      const movieDetails = details === false ? titulo : {anoLancamento, avaliacao, diretor, id, titulo}
+
+      acc[genero] ? acc[genero].push(movieDetails) : acc[genero] = [movieDetails]
+      
+      details === false ? acc[genero].sort() : acc[genero].sort((a,b)=> a.titulo.localeCompare(b.titulo))
+    })
+  return acc
+  }, {})
 }
 
 module.exports = {
