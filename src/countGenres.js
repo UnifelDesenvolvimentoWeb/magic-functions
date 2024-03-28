f8adddedcdf32081305413f36eab4d59e259bae5
maila520 <mailasantos520@gmail.com>
2024-03-28 17:32:04 -0300

    desafio 10 ok

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,9 +1,28 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+  if (movies.length === 0) {
+    return {};
+  }
+  const genreCounts = {};
+
+  movies.forEach(movie => {
+    if(movie.genero && movie.genero.length > 0){
+      movie.genero.forEach(genero => {
+        if(genreCounts.hasOwnProperty(genero)){
+          genreCounts[genero]++;
+        }
+        else{
+          genreCounts[genero] = 1;
+        }
+      })
+    }
+  });
+  return genreCounts
 }
 
-module.exports = {
-  countGenres
-}
+    console.log(countGenres(movies))
+
+    module.exports = {
+      countGenres
+    }
